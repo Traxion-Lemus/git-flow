10ad97c61836dda4fa47a1bc9c076168445cd4cb
lemus <o.lemus@traxion.global>
2023-07-25 12:14:39 -0600

    feat: add division operation
    Applies git rebase develop
    
    Task #5

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ console.log(basic.addition(8, 9, 10));
 console.log(basic.subtraction(20, 18))
 
 console.log(basic.multiplication(3, 6))
+
+const divisonResults = basic.division(16, 3);
+
+console.log(`The quotient is ${divisonResults.quotient} and the remainder is ${divisonResults.remainder}`);
