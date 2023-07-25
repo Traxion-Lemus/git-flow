1008e2c25316cd16533213a2c72efb2b2b92049c
lemus <o.lemus@traxion.global>
2023-07-25 11:50:50 -0600

    feat: Add filter to addition operation
    Task #2

--- operations.js
+++ operations.js
@@ -1,6 +1,15 @@
 export class BasicOperations {
+
+    /**
+     * Takes n numbers and returns the sum, also filters the input in order to avoid an error
+     *
+     * @param x
+     * @returns {*}
+     */
     sum(...x) {
-        return x.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+        return x
+            .filter(value => !isNaN(value))
+            .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
     }
 }
 
