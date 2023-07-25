d2dc1764c8a81a31b3d3bcf8c375745a5b6f8afc
lemus <o.lemus@traxion.global>
2023-07-25 12:01:14 -0600

    feat: Add addition operation
    Task #2

--- operations.js
+++ operations.js
@@ -3,11 +3,11 @@ export class BasicOperations {
     /**
      * Takes n numbers and returns the sum, also filters the input in order to avoid an error
      *
-     * @param x
-     * @returns {*}
+     * @param addends
+     * @returns {*} sum
      */
-    sum(...x) {
-        return x
+    addition(...addends) {
+        return addends
             .filter(value => !isNaN(value))
             .reduce((previousValue, currentValue) => previousValue + currentValue, 0);
     }
