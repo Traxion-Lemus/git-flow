76a89363e1cd3407f1b0afbadd3ed2d6b9720c5e
Oscar Lemus <137565875+Traxion-Lemus@users.noreply.github.com>
2023-09-07 11:02:52 -0600

    feat: [#12] add power operation (#14)

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ console.log(basic.subtraction(20, 18))
 
 console.log(basic.multiplication(3, 6))
 
+console.log(basic.power(3, 4))
+
 const divisonResults = basic.division(16, 3);
 
 console.log(`The quotient is ${divisonResults.quotient} and the remainder is ${divisonResults.remainder}`);
